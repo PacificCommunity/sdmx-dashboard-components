740b3ef73000064d05a2468840f471fec97799e2
stanozr <stanislaso.spc.int>
2023-12-06 21:26:00 +1100

    Cleanup and comment index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 
-import Dashboard from './components/dashboard';
-// import reportWebVitals from './reportWebVitals';
+import Dashboard from './components/dashboard'
 
+// Get all nodes with CSS classname sdmx-dashboard-react
 const elems = document.getElementsByClassName('sdmx-dashboard-react')
 
-// loop through collections of elements and create a root for each
+// loop through collections of elements and render Dashboards
 Array.from(elems).forEach((elem) => {
   // get URL from element data attribute and pass to App
   const url = elem.getAttribute('data-url')
+  // create root and render
+  // if URL to config file is not provided, display a message
   ReactDOM.createRoot(elem as HTMLElement).render(
     url?.length > 0 &&
     <React.StrictMode>

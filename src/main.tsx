d16fe09ef72fdaa2d282a30b727d1c0c43263c15
Thomas Tilak <thomas.tilak@gmail.com>
2025-04-23 15:23:33 +1100

    switch to HashRouter for Github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter, Route, Routes} from "react-router";
+import {HashRouter, Route, Routes} from "react-router";
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename={process.env.NODE_ENV === 'production' ? '/sdmx-dashboard-components' : '/'}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/:active_tab" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )

5828f000c13fa36e5bba099b0c0bc90bb4f35886
Thomas Tilak <thomas.tilak@gmail.com>
2025-04-23 14:04:31 +1100

    fixes for deployment

--- vite.preview.ts
+++ vite.preview.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/sdmx-dashboard-components/' : '/',
   plugins: [
     react(),
   ],

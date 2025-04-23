31efdd0a79e09680eafe0c3f7aba7e667926db2c
Thomas Tilak <thomas.tilak@gmail.com>
2025-04-23 13:40:15 +1100

    add deploy preview script

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/sdmx-dashboard-components/' : '/',
   plugins: [
     react(),
     libInjectCss(),

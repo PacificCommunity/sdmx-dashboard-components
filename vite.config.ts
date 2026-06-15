8b5f84d336b01221846f8d4bade33ad38f5aa729
Thomas Tilak <thomas.tilak@gmail.com>
2026-06-15 19:08:41 +1100

    move from vite to tsdown to build library

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,11 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import { libInjectCss } from 'vite-plugin-lib-inject-css'
-
-import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    libInjectCss()
+    libInjectCss(),
   ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'lib/index.ts'),
-      formats: ['es']
-    },
-    rollupOptions: {
-      external: [ ...Object.keys(pkg.peerDependencies), /^highcharts\/modules\/.*/]
-    },
-    copyPublicDir: false
-  }
 })
